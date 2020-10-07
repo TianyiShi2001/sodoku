089ff12647f140ed457fe61237d84878355e4a14
Tianyi <ShiTianyi2001@outlook.com>
2020-10-07 20:40:45 +0600

    .

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,3 @@
-
-// use sodoku::{SodokuMatrix, Sodoku};
-// use sodoku::examples::SODOKU;
-
-
-// pub fn main() {
-//     for sodoku in SODOKU.iter() {
-//         let mut sodoku = Sodoku::from_matrix(sodoku.clone());
-//         sodoku.solve();
-//         println!("{}", sodoku);
-//     }
-// }
+fn main() {
+    
+}
